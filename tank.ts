c4b6cc9924f21b1800828af380bc1b924472bb59
gcca <gcca@gcca.tk>
2017-11-26 23:05:19 -0500

    Actions mapping

--- tank.ts
+++ tank.ts
@@ -1,26 +1,32 @@
+type Position = [number, number];
+
 export class Tank {
 
   private static STEP = 1;
   private static Y = 1;
 
-  private static DEFAULT_POSITION: [number, number] = [0, 0];
+  private static DEFAULT_POSITION = [0, 0] as Position;
 
-  private position: [number, number];
+  private position: Position;
+
+  private actions: {[order: string]: () => void};
 
   constructor() {
-    this.position = Tank.DEFAULT_POSITION;
+    this.position = Tank.DEFAULT_POSITION.slice() as Position;
+
+    this.actions = {
+      'b': () => this.backward(),
+      'f': () => this.forward(),
+    };
   }
 
   public execute(orders: string[]): void {
     for (const order of orders) {
-      switch (order) {
-        case 'b': this.backward(); break;
-        case 'f': this.forward(); break;
-      }
+      this.actions[order]();
     }
   }
 
-  public where_i_am(): [number, number] {
+  public where_i_am(): Position {
     return this.position;
   }
 
